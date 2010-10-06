665e2e4176ab4e94bd8cc7e3d25731317b409f59
Michael Snoyman <michael@snoyman.com>
2010-10-06 15:43:46 +0200

    Threaded FastCGI

--- fastcgi.hs
+++ fastcgi.hs
@@ -1,6 +1,7 @@
 import Controller
-import Network.Wai.Handler.FastCGI (run)
+import Network.Wai.Handler.FastCGI (runFork)
 import Network.HTTP.Enumerator (withHttpEnumerator)
+import Control.Concurrent (forkIO)
 
 main :: IO ()
-main = withHttpEnumerator $ withHaskellers run
+main = withHttpEnumerator $ withHaskellers $ runFork Nothing forkIO 5
