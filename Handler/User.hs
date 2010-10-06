1ab2fe2e7f1af872cbd7e361fee6bc56f468e635
Michael Snoyman <michael@snoyman.com>
2010-10-06 10:45:36 +0200

    User pages

--- Handler/User.hs
+++ Handler/User.hs
@@ -5,8 +5,71 @@ module Handler.User
 
 import Haskellers
 import Handler.Root (gravatar)
+import Data.Ord (comparing)
+import Data.List (sortBy)
+import Data.Maybe (fromMaybe)
+import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 as S8
+import qualified Data.ByteString.UTF8 as SU
+import OpenSSL.Cipher
+import OpenSSL.EVP.Base64
+import System.IO.Unsafe (unsafePerformIO)
+import Yesod.Form.Jquery (urlJqueryJs)
 
 getUserR :: UserId -> Handler RepHtml
 getUserR uid = do
     u <- runDB $ get404 uid
-    defaultLayout $(hamletFile "user")
+    skills <- runDB $ do
+        x <- selectList [UserSkillUserEq uid] [] 0 0
+        y <- mapM (get404 . userSkillSkill . snd) x
+        return
+            $ map skillName
+            $ sortBy (comparing skillOrder) y
+    let email = fromMaybe "fake@email.com" $ userEmail u
+    y <- getYesod
+    defaultLayout $ do
+        setTitle $ string $ "Haskellers profile for " ++ userFullName u
+        addStyle $(cassiusFile "user")
+        addScriptEither $ urlJqueryJs y
+        addJavascript $(juliusFile "user")
+        $(hamletFile "user")
+  where
+    notOne 1 = False
+    notOne _ = True
+
+mailhidePublic :: String
+mailhidePublic = "01_o4fjI3uXdNz6rLrIquvlw=="
+
+mailhidePrivate :: S.ByteString
+mailhidePrivate = S8.pack "\x42\x40\x54\x79\x07\x8c\x47\xb0\x50\xd7\x9a\x33\xc6\x09\x69\x1c"
+
+emailLink :: String -> String
+emailLink email = unsafePerformIO $ do
+    enc <- encryptAddress email
+    return $ concat
+        [ "http://www.google.com/recaptcha/mailhide/d?k="
+        , mailhidePublic
+        , "&c="
+        , enc
+        ]
+
+encryptAddress :: String -> IO String
+encryptAddress =
+    fmap (map b64Url . S8.unpack . encodeBase64BS) . encrypt . pad
+  where
+    b64Url '+' = '-'
+    b64Url '/' = '_'
+    b64Url c   = c
+
+pad :: String -> S.ByteString
+pad s =
+    let bs' = SU.fromString s
+        blockSize = 16
+        numpad = blockSize - (S.length bs' `mod` blockSize)
+        padding = S.replicate numpad $ fromIntegral numpad
+     in bs' `S.append` padding
+
+encrypt :: S.ByteString -> IO S.ByteString
+encrypt bs = do
+    ctx <- newAESCtx Encrypt mailhidePrivate $ S.replicate 16 0
+    aesCBC ctx bs
