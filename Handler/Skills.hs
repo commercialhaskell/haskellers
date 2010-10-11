6548949392c92fe8883483e1d51450614a27da44
Michael Snoyman <michael@snoyman.com>
2010-10-11 22:19:50 +0200

    Hide less common skills

--- Handler/Skills.hs
+++ Handler/Skills.hs
@@ -20,17 +20,26 @@ postAllSkillsR = do
 
 getAllSkillsR :: Handler RepHtmlJson
 getAllSkillsR = do
-    skills <- runDB $ selectList [] [SkillNameAsc] 0 0 >>= mapM (\(sid, s) -> do
+    skills' <- runDB $ selectList [] [SkillNameAsc] 0 0 >>= mapM (\(sid, s) -> do
         users <- count [UserSkillSkillEq sid]
         return ((sid, s), users)
         )
+    showall <- runFormGet' $ boolInput "show-all"
+    let threshhold = 10
+    let skills =
+            if showall
+                then skills'
+                else filter (\(_, x) -> x >= threshhold) skills'
+    let hidden = length skills' - length skills
+    let areHidden = hidden > 0
+    let showAllUrl = (AllSkillsR, [("show-all", "true")])
     render <- getUrlRender
     defaultLayoutJson (do
         setTitle "Browse all skills"
         addStyle $(cassiusFile "skills")
         $(hamletFile "skills")
         ) $ jsonMap
-        [ ("skills", jsonList $ flip map skills $ \((sid, Skill name), users) ->
+        [ ("skills", jsonList $ flip map skills' $ \((sid, Skill name), users) ->
             jsonMap
                 [ ("id", jsonScalar $ showIntegral sid)
                 , ("name", jsonScalar name)
