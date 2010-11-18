7c1c315a003e6d5644beca41c89edd080794ef4d
Michael Snoyman <michael@snoyman.com>
2010-11-18 14:32:00 +0200

    persistent 0.3.1

--- Model.hs
+++ Model.hs
@@ -44,22 +44,22 @@ instance ToHtml Employment where toHtml = string . prettyEmployment
 share2 mkPersist (mkMigrate "migrateAll") [$persist|
 User
     fullName String Asc id=full-name
-    website String null id=website
-    email String null toFormField=maybeEmailField id=email update
-    verifiedEmail Bool default=false Eq update
-    verkey String null update
-    haskellSince Int null Asc
-    desc Textarea null id=desc
+    website String Maybe id=website
+    email String Maybe toFormField=maybeEmailField id=email Update
+    verifiedEmail Bool default=false Eq Update
+    verkey String Maybe Update
+    haskellSince Int Maybe Asc
+    desc Textarea Maybe id=desc
     visible Bool default=true Eq
-    real Bool default=false update Desc Eq
-    realPic Bool default=false update Eq
-    admin Bool default=false update
-    employment Employment null
-    blocked Bool update Eq default=false
+    real Bool default=false Update Desc Eq
+    realPic Bool default=false Update Eq
+    admin Bool default=false Update
+    employment Employment Maybe
+    blocked Bool Update Eq default=false
     emailPublic Bool default=false
-    location String null
-    longitude Double null Ne
-    latitude Double null Ne
+    location String Maybe
+    longitude Double Maybe Ne
+    latitude Double Maybe Ne
 Username
     user UserId
     username String
@@ -80,10 +80,10 @@ Package
     name String Asc
     UniquePackage user name
 Message
-    closed Bool update Eq
+    closed Bool Update Eq
     when UTCTime Asc
-    from UserId null
-    regarding UserId null
+    from UserId Maybe
+    regarding UserId Maybe
     text Textarea
 News
     when UTCTime Desc
@@ -111,7 +111,7 @@ Team
 TeamUser
     team TeamId Eq
     user UserId Eq
-    status TeamUserStatus update
+    status TeamUserStatus Update
     UniqueTeamUser team user
 TeamNews
     team TeamId Eq In
@@ -124,19 +124,19 @@ TeamPackage
     team TeamId Eq
     name String Asc
     hackage Bool
-    desc String null
-    homepage String null
+    desc String Maybe
+    homepage String Maybe
 Topic
     team TeamId Eq
     created UTCTime Desc
     type TopicType
-    status TopicStatus update
-    creator UserId null update Eq
+    status TopicStatus Update
+    creator UserId Maybe Update Eq
     title String
 TopicMessage
     topic TopicId Eq
     created UTCTime Asc
-    creator UserId null update Eq
+    creator UserId Maybe Update Eq
     content Html
     deriving Show Eq
 |]
