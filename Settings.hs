27dffcdbcb8b1898e4e0f82acbddb41d6fde977d
Amitai Burstein <amitai@gizra.com>
2016-08-28 23:30:34 +0300

    Remove mySql related code

--- Settings.hs
+++ Settings.hs
@@ -96,16 +96,6 @@ instance FromJSON AppSettings where
         appCopyright              <- o .: "copyright"
         appAnalytics              <- o .:? "analytics"
 
-        -- This code enables MySQL's strict mode, without which MySQL will truncate data.
-        -- See https://github.com/yesodweb/persistent/wiki/Database-Configuration#strict-mode for details
-        -- If you choose to keep strict mode enabled, it's recommended that you enable it in your my.cnf file so that it's also enabled for your MySQL console sessions.
-        -- (If you enable it in your my.cnf file, you can delete this code).
-        let appDatabaseConf = fromYamlAppDatabaseConf { myConnInfo = (myConnInfo fromYamlAppDatabaseConf) {
-                MySQL.connectOptions =
-                  ( MySQL.connectOptions (myConnInfo fromYamlAppDatabaseConf)) ++ [MySQL.InitCommand "SET SESSION sql_mode = 'STRICT_ALL_TABLES';\0"]
-              }
-            }
-
         let appAwsCreds      = (,)  fromYamlAppAwsConf
         let appFacebookCreds = (,,) fromYamlAppFacebookConf
         let appGoogleCreds   = (,)  fromYamlAppGoogleConf
