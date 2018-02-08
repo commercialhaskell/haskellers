3363a54eda9f86861193daf47349e7195c30faba
Michael Snoyman <michael@snoyman.com>
2018-02-08 10:59:37 +0200

    File cleanup

--- fastcgi.hs
+++
@@ -1,7 +0,0 @@
-import Controller
-import Network.Wai.Handler.FastCGI (runFork)
-import Network.HTTP.Enumerator (withHttpEnumerator)
-import Control.Concurrent (forkIO)
-
-main :: IO ()
-main = withHttpEnumerator $ withHaskellers $ runFork Nothing forkIO 5
