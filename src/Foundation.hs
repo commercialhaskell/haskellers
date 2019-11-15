698808447eb19b9a92723abd2d32ebc153097398
Michael Snoyman <michael@snoyman.com>
2019-11-15 09:17:00 +0530

    moar summer

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -364,7 +364,8 @@ instance YesodPersist App where
 
 instance YesodJquery App where
     urlJqueryUiCss _ = Left $ StaticR jquery_ui_css
-instance YesodSummernote App
+instance YesodSummernote App where
+    summernoteLoadLibrariesAndCss = True
 
 -- This instance is required to use forms. You can modify renderMessage to
 -- achieve customized and internationalized form validation messages.
